aa044c3e0697fb85a7e4c762b62ece8a26cd340e
denni.kurniawan <denni.kurniawan@jatis.com>
2020-01-23 00:48:58 +0700

    dd

--- src/server.js
+++ src/server.js
@@ -1,19 +1,16 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname + 'index.html'));
-});
-
 // Start the app by listening on the default Heroku port
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log('Express server listening on port', port)
+app.listen(process.env.PORT || 8080);
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
+
+console.log('Console listening!');
